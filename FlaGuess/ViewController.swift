426865fdd89dd2e7426679f308bb6dea1134e418
Mihail87 <leontemihai@gmail.com>
2019-08-26 15:49:25 +0300

    Project & Challanges completed.

--- FlaGuess/ViewController.swift
+++ FlaGuess/ViewController.swift
@@ -9,11 +9,82 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var round = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
+    
+    func askQuestion(alert: UIAlertAction! = nil) {
+        round += 1
+        
+        if round == 11 {
+            title = "Final Score: \(score) "
+            
+            let ac = UIAlertController(title: "Game Over", message: "Your final score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: askQuestion))
+            present(ac, animated: true, completion: nil)
+            
+        } else {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+            
+            button1.setImage(UIImage(named: countries[0]), for: .normal)
+            button2.setImage(UIImage(named: countries[1]), for: .normal)
+            button3.setImage(UIImage(named: countries[2]), for: .normal)
+            
+            title = "\(countries[correctAnswer].uppercased())? Score: \(score) "
+        }
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message: String
+        
+        print("sender.tag is \(sender.tag)")
+        print("correctAnswer is \(correctAnswer)")
+        
+        if sender.tag == correctAnswer {
+            score += 1
+            
+            title = "Correct"
+            message = "Your new score is \(score)"
+        } else {
+            score -= 1
+            
+            title = "Wrong"
+            message = "That’s actually the flag of \(countries[sender.tag].uppercased()). Your new score is \(score)"
+        }
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true, completion: nil)
+        
+    }
+    
 
 
 }
